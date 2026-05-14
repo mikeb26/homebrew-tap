b79ae75b700b6ea458c0620c1082b9ddbdc73977
Mike Brown <github@torvosoft.com>
2026-05-14 17:36:51 -0400

    spotsh: update to v0.20.3

--- Formula/spotsh.rb
+++ Formula/spotsh.rb
@@ -1,8 +1,8 @@
 class Spotsh < Formula
   desc "Launch disposable AWS EC2 Spot shell boxes and SSH into them"
   homepage "https://github.com/mikeb26/spotsh"
-  url "https://github.com/mikeb26/spotsh/archive/refs/tags/v0.20.2.tar.gz"
-  sha256 "f5311161b4dd089a298e0246b4633a672cf6cf55f64e7496ba692c01d1261b66"
+  url "https://github.com/mikeb26/spotsh/archive/refs/tags/v0.20.3.tar.gz"
+  sha256 "55260d42c26bb7e0865c1c9eebd9547f738b62a5e4dc8b4b6ee5d6bcacc52d7a"
   license "AGPL-3.0-only"
   head "https://github.com/mikeb26/spotsh.git", branch: "main"
 
@@ -11,7 +11,7 @@ class Spotsh < Formula
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-    File.write buildpath/"cmd/spotsh/version.txt", "v0.20.2b"
+    File.write buildpath/"cmd/spotsh/version.txt", "v0.20.3b"
     system "make", "vendor"
     system "make", "build"
     bin.install "spotsh"
