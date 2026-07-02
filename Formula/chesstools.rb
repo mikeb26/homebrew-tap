24c797275d669efda6383b10a51ec76a7bdf7b79
Mike Brown <github@torvosoft.com>
2026-07-02 15:10:35 -0400

    chesstools v0.10.5

--- Formula/chesstools.rb
+++ Formula/chesstools.rb
@@ -1,8 +1,8 @@
 class Chesstools < Formula
   desc "Chess Tools"
   homepage "https://github.com/mikeb26/chesstools"
-  url "https://github.com/mikeb26/chesstools/archive/refs/tags/v0.10.4.tar.gz"
-  sha256 "9df26ac0eaff8925ec14462f7f9bc2d51681da9dd79095bca1dcadeafc01aab4"
+  url "https://github.com/mikeb26/chesstools/archive/refs/tags/v0.10.5.tar.gz"
+  sha256 "f14d0e418af9e8bb9bad4600c1e3556af6024e0fbfed9ec32a2a02832147b3d8"
   license "MIT"
   head "https://github.com/mikeb26/chesstools.git", branch: "main"
 
@@ -11,7 +11,7 @@ class Chesstools < Formula
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-    File.write buildpath/"cmd/ct/version.txt", "v0.10.4b"
+    File.write buildpath/"cmd/ct/version.txt", "v0.10.5b"
     system "make", "build"
     bin.install "ct"
   end
